0f0864ec0009018231f264b5c115481d8636b9d7
Saiful264 <saifulislamx264@gmail.com>
2024-04-29 21:17:39 +0600

    Project inti

---
+++ src/lib/theme/theme.ts
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1586FD",
+    },
+    secondary: {
+      main: "#666f73",
+    },
+  },
+});
